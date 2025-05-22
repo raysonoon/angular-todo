2a6d26d5f3be3ebb3ebec28e085854b35e5f16f6
raysonoon <raysonoonhere@gmail.com>
2025-05-22 11:28:29 +0800

    Implement todo structure and functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
+  imports: [CommonModule],
 })
 export class AppComponent {
-  title = 'todo';
+  componentTitle = "My To Do List";
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  addItem(description: string) {
+    if (!description) return;
+
+    this.allItems.unshift({
+      description,
+      done: false,
+    });
+  }
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done,
+    );
+  }
 }
